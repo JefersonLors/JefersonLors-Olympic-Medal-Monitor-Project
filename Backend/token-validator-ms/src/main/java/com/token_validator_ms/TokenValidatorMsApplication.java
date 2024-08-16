e47c19c166a0726ca3d0079e682feb4c87a5e360
Jeferson Lors <lors.jeferson@gmail.com>
2024-08-16 16:04:46 -0300

    Configuração do validator

---
+++ Backend/token-validator-ms/src/main/java/com/token_validator_ms/TokenValidatorMsApplication.java
@@ -0,0 +1,13 @@
+package com.token_validator_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenValidatorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenValidatorMsApplication.class, args);
+	}
+
+}
