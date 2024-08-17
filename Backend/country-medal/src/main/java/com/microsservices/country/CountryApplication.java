5bdb156fe1b39929e7bb37604bb318b8d6691915
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-08-17 20:39:36 -0300

    adicionando dependencias para eureka cliente e open feing

--- Backend/country-medal/src/main/java/com/microsservices/country/CountryApplication.java
+++ Backend/country-medal/src/main/java/com/microsservices/country/CountryApplication.java
@@ -2,8 +2,12 @@ package com.microsservices.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CountryApplication {
 
 	public static void main(String[] args) {
