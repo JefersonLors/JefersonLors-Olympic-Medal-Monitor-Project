111eee46400e01a60501a387f558a0ff8ce4e292
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-08-17 18:38:10 -0300

    atualizando dtos para ids não criptografados

--- Backend/country-medal/src/main/java/com/microsservices/country/dtos/MedalDto.java
+++ Backend/country-medal/src/main/java/com/microsservices/country/dtos/MedalDto.java
@@ -12,13 +12,22 @@ public class MedalDto {
     }
     public MedalDto(Medal m) {
         try{
-            this.id = criptografiaAES.encrypt(m.getId().toString());
+            this.id = m.getId().toString();
             this.type = m.getType();
         }catch(Exception e){
-            throw new IllegalArgumentException("build error Country");
+            throw new RuntimeException("build error Country");
         }
     }
 
+    public MedalDto encryptId(){
+        try {
+            this.id = criptografiaAES.encrypt(this.id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        return this;
+    }
     
     public MedalDto(String id, MedalType type) {
         this.id = id;
