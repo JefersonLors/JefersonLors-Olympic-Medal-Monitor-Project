607453d22f0da2ae7b1191949d06b62ce0bbd944
Jeferson Lors <lors.jeferson@gmail.com>
2024-08-19 10:20:20 -0300

    baixando atualizações da dev

---
+++ Backend/notifier-ms/src/test/java/com/notifier_ms/NotifierMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifier_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifierMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
