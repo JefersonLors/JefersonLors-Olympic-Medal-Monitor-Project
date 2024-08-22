d3f5464fe3c4898a600c67f25fcebd26a211a009
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-08-21 22:47:23 -0300

    request mapping

--- Backend/country-medal/src/main/java/com/microsservices/country/controllers/SportController.java
+++ Backend/country-medal/src/main/java/com/microsservices/country/controllers/SportController.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
-@RestController("/sport")
+@RestController()
+@RequestMapping("/sport")
 public class SportController {
     @Autowired
     SportService service;
