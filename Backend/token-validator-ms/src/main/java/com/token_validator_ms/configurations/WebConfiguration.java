450a214a3042e258534a4582f18e5e3c6d205018
Jeferson Lors <lors.jeferson@gmail.com>
2024-08-22 16:04:39 -0300

    Adição da configuração de CORS na API de validação de token

--- Backend/token-validator-ms/src/main/java/com/token_validator_ms/configurations/WebConfiguration.java
+++ Backend/token-validator-ms/src/main/java/com/token_validator_ms/configurations/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.authentication_ms.configuration;
+package com.token_validator_ms.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
