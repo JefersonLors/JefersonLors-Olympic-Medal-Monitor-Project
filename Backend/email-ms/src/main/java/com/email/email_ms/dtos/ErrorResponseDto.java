420c58c743ba7e3f5e5ea1ef779a46ac0b46716d
Jeferson Lors <lors.jeferson@gmail.com>
2024-08-22 12:34:19 -0300

    Inserção de configuração para centralizar o tratamento de exceções nos micro serviços de e-mail e usuário

---
+++ Backend/email-ms/src/main/java/com/email/email_ms/dtos/ErrorResponseDto.java
@@ -0,0 +1,15 @@
+package com.email.email_ms.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ErrorResponseDto {
+    public String statusCode;
+    public String message;
+}
