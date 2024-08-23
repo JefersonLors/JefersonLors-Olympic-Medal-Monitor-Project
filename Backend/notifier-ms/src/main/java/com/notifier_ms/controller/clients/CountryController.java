90653d22c8abdb093dbd6f686e9e1810672ef5b0
Jeferson Lors <lors.jeferson@gmail.com>
2024-08-23 09:18:37 -0300

    Correções no serviço de notificação

--- Backend/notifier-ms/src/main/java/com/notifier_ms/controller/clients/CountryController.java
+++ Backend/notifier-ms/src/main/java/com/notifier_ms/controller/clients/CountryController.java
@@ -2,9 +2,11 @@ package com.notifier_ms.controller.clients;
 
 import com.notifier_ms.dto.CountryDto;
 import com.notifier_ms.dto.MedalDto;
+import com.notifier_ms.dto.SportDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,4 +18,6 @@ public interface CountryController {
     @RequestMapping(method= RequestMethod.GET, value="/country/ById/{id}")
     ResponseEntity<CountryDto> getCountryById(@PathVariable Long id);
 
+    @RequestMapping(method = RequestMethod.GET, value="/sport/{id}")
+    ResponseEntity<SportDto> getSportById(@PathVariable Long id);
 }
