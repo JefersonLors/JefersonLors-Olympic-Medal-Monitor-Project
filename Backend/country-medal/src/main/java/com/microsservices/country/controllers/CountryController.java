85d42e64d18c0c112b9cfb24428b79d483b2ec10
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-08-23 11:36:36 -0300

    passando uma lista de roles

--- Backend/country-medal/src/main/java/com/microsservices/country/controllers/CountryController.java
+++ Backend/country-medal/src/main/java/com/microsservices/country/controllers/CountryController.java
@@ -37,7 +37,7 @@ public class CountryController {
     @Operation(summary="Retorna todos os países.", 
     description="Retorna os países e a quantidade de cada tipo de medalha.")
     public ResponseEntity<List<CountryMedalDto>> getCountrys(@RequestHeader("Authorization") String requestHeader) {
-        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER))))
+        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER, Role.ROLE_ADMIN))))
             return service.getCountrys();
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
@@ -46,7 +46,7 @@ public class CountryController {
     @Operation(summary = "Busca o país por nome.", 
     description = "Retorna o país e as medalhas ganhas em cada esporte.")
     public ResponseEntity<CountryMedalInSportsDto> getCountry(@RequestHeader("Authorization") String requestHeader, @PathVariable String name) {
-        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER))))
+        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER, Role.ROLE_ADMIN))))
             return service.getCountry(name);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
@@ -55,7 +55,7 @@ public class CountryController {
     @Operation(summary = "Busca o país atravez do id criptografado.", 
     description = "Recebe o id do país criptografado com o aulgoritimo de criptografia AES e retorna o país e as medalhas ganhas em cada esporte.")
     public ResponseEntity<CountryDto> getCountryByEncryptedId(@RequestHeader("Authorization") String requestHeader, @PathVariable String id) {
-        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER))))
+        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER, Role.ROLE_ADMIN))))
             return service.getCountryByEncryptedId(id);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
@@ -63,7 +63,7 @@ public class CountryController {
     @GetMapping("/ById/{id}")
     @Operation(summary = "Busca o país por id" , description = "Retorna a entidade país")
     public ResponseEntity<CountryDto> getCountryById(@RequestHeader("Authorization") String requestHeader, @PathVariable Long id) {
-        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER))))
+        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER, Role.ROLE_ADMIN))))
             return service.getCountryById(id);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         
@@ -72,7 +72,7 @@ public class CountryController {
     @GetMapping("/WithMedalsById/{id}")
     @Operation(summary = "Busca o país por id" , description = "Retorna a entidade país com todas as suas medalhas")
     public ResponseEntity<CountryMedalInSportsDto> getCountryWithMedalsById(@RequestHeader("Authorization") String requestHeader, @PathVariable Long id) {
-        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER))))
+        if(this.roleValidationService.currentUserHasRole(requestHeader, new ArrayList<>(List.of(Role.ROLE_USER, Role.ROLE_ADMIN))))
             return service.getCountryWithMedalsById(id);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         
